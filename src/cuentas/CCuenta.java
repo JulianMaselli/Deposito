01da7aa5cedb99c47602531435702a88c31484c2
JulianMaselli <julianaresarevalo@gmail.com>
2025-03-03 19:19:12 +0100

    Ejemplo prueba uso ED04

--- src/cuentas/CCuenta.java
+++ src/cuentas/CCuenta.java
@@ -1,5 +1,9 @@
 package cuentas;
 
+/**
+ * Clase que representa cuenta bancaria con un saldo y un tipo de interés
+ * Se pueden realizar operaciones ingresar y retirar
+ */
 public class CCuenta {
 
 
@@ -12,6 +16,12 @@ public class CCuenta {
     {
     }
 
+    /**
+     * Constructor que inicializa los valores de la cuenta con los parámetros proporcionados
+     * @param nom El nombre del titular de la cuenta
+     * @param cue El número de cuenta
+     * @param sal El saldo inicial de la cuenta
+     */
     public CCuenta(String nom, String cue, double sal, double tipo)
     {
         nombre =nom;
@@ -19,10 +29,18 @@ public class CCuenta {
         saldo=sal;
     }
 
+    /**
+     * Obtiene el nombre del titular de la cuenta
+     * @return El nombre del titular de la cuenta
+     */
     public String getNombre() {
         return nombre;
     }
 
+    /**
+     * Establece el nombre del titular de la cuenta
+     * @param nombre El nuevo nombre del titular
+     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
@@ -51,11 +69,20 @@ public class CCuenta {
         this.tipoInterés = tipoInterés;
     }
 
+    /**
+     * Devuelve el estado actual de la cuenta
+     * @return El saldo de la cuenta
+     */
     public double estado()
     {
         return getSaldo();
     }
 
+    /**
+     * Ingresa una cantidad de dinero en la cuenta. Cantidad negativa - lanza excepción
+     * @param cantidad La cantidad a ingresar en la cuenta
+     * @throws Exception Si la cantidad es negativa, se lanza una excepción
+     */
     public void ingresar(double cantidad) throws Exception
     {
         if (cantidad<0)
@@ -63,6 +90,11 @@ public class CCuenta {
         setSaldo(getSaldo() + cantidad);
     }
 
+     /**
+     * Retira una cantidad de dinero de la cuenta. Si la cantidad es negativa o el saldo es insuficiente, lanza una excepción
+     * @param cantidad La cantidad a retirar de la cuenta
+     * @throws Exception Si la cantidad es negativa o el saldo es insuficiente, se lanza una excepción
+     */
     public void retirar(double cantidad) throws Exception
     {
         if (cantidad <= 0)
